cc0088c2ce8713e77f2a433771cfa48582e81b18
Adam Kafka <adkafka@gmail.com>
2017-04-12 11:11:43 -0400

    Removed unnecessary braces

--- pthread_interpose.c
+++ pthread_interpose.c
@@ -18,26 +18,107 @@ __attribute__((destructor))  void fini(void) {
 
 #undef pthread_mutex_init
 int pthread_mutex_init(pthread_mutex_t * mutex, const pthread_mutexattr_t *mutexattr){
-{
     int rc;
-    static int (*real_create)(pthread_mutex_t * mutexx, const pthread_mutexattr_t *mutexattr);
-    static bool use_standard_pthread_mutex_init = false;
-
+    static int (*real_create)(pthread_mutex_t * mutex, const pthread_mutexattr_t *mutexattr);
     if (!real_create){
         const void* addr{dlsym(RTLD_NEXT, "pthread_mutex_init")};
         std::memcpy(&real_create,&addr, sizeof(addr));
     }
 
-    if(use_standard_pthread_mutex_init==true)
-        return real_create(mutex,mutexattr);
-
     rc = real_create(mutex, mutexattr);
-    if(!rc) {
-        //store_id(thread);
+    if(!rc)
         backtrace();
-    }
     return rc;
 }
-}
+
+#undef pthread_mutex_lock
+int pthread_mutex_lock(pthread_mutex_t * mutex){
+    int rc;
+    static int (*real_create)(pthread_mutex_t * mutex);
+    if (!real_create){
+        const void* addr{dlsym(RTLD_NEXT, "pthread_mutex_lock")};
+        std::memcpy(&real_create,&addr, sizeof(addr));
+    }
+
+    rc = real_create(mutex);
+    if(!rc)
+        backtrace();
+    return rc;
 }
 
+#undef pthread_mutex_trylock
+int pthread_mutex_trylock(pthread_mutex_t * mutex){
+    int rc;
+    static int (*real_create)(pthread_mutex_t * mutex);
+    if (!real_create){
+        const void* addr{dlsym(RTLD_NEXT, "pthread_mutex_trylock")};
+        std::memcpy(&real_create,&addr, sizeof(addr));
+    }
+
+    rc = real_create(mutex);
+    if(!rc)
+        backtrace();
+    return rc;
+}
+
+#undef pthread_mutex_timedlock
+int pthread_mutex_timedlock(pthread_mutex_t * mutex, const struct timespec *abs_timeout){
+    int rc;
+    static int (*real_create)(pthread_mutex_t * mutex, const struct timespec *abs_timeout);
+    if (!real_create){
+        const void* addr{dlsym(RTLD_NEXT, "pthread_mutex_timedlock")};
+        std::memcpy(&real_create,&addr, sizeof(addr));
+    }
+
+    rc = real_create(mutex,abs_timeout);
+    if(!rc)
+        backtrace();
+    return rc;
+}
+
+#undef pthread_mutex_unlock
+int pthread_mutex_unlock(pthread_mutex_t * mutex){
+    int rc;
+    static int (*real_create)(pthread_mutex_t * mutex);
+    if (!real_create){
+        const void* addr{dlsym(RTLD_NEXT, "pthread_mutex_unlock")};
+        std::memcpy(&real_create,&addr, sizeof(addr));
+    }
+
+    rc = real_create(mutex);
+    if(!rc)
+        backtrace();
+    return rc;
+}
+
+#undef pthread_mutex_consistent
+int pthread_mutex_consistent(pthread_mutex_t * mutex){
+    int rc;
+    static int (*real_create)(pthread_mutex_t * mutex);
+    if (!real_create){
+        const void* addr{dlsym(RTLD_NEXT, "pthread_mutex_consistent")};
+        std::memcpy(&real_create,&addr, sizeof(addr));
+    }
+
+    rc = real_create(mutex);
+    if(!rc)
+        backtrace();
+    return rc;
+}
+
+#undef pthread_mutex_destroy
+int pthread_mutex_destroy(pthread_mutex_t * mutex){
+    int rc;
+    static int (*real_create)(pthread_mutex_t * mutex);
+    if (!real_create){
+        const void* addr{dlsym(RTLD_NEXT, "pthread_mutex_destroy")};
+        std::memcpy(&real_create,&addr, sizeof(addr));
+    }
+
+    rc = real_create(mutex);
+    if(!rc)
+        backtrace();
+    return rc;
+}
+
+}
