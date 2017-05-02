7658ce4b087d9adb999d4767484018393457912f
Adam Kafka <adkafka@gmail.com>
2017-05-02 12:01:58 -0400

    Updated cond_vars

--- pthread_test.cpp
+++ pthread_test.cpp
@@ -4,34 +4,50 @@
 #include <mutex>
 #include <thread>
 
+#include <chrono>//seconds
+#include <thread>//sleep_for
+
 //#include "spin_lock.hpp"
+
+#define CACHE_SIZE 64
+#define CACHE_ALIGNED __attribute__((aligned(CACHE_SIZE)))
 
 /* COND VAR */
 
-bool ready(false);
-std::condition_variable cv;
-std::mutex mtx;
+struct data{
+    volatile bool ready;
+    char pad1[CACHE_SIZE-sizeof(bool)];
+    std::condition_variable cv;
+    char pad2[CACHE_SIZE-sizeof(std::condition_variable)];
+    std::mutex mtx;
+    char pad3[CACHE_SIZE-sizeof(std::mutex)];
+} CACHE_ALIGNED;
 
-void print_id (int id) {
-    std::unique_lock<std::mutex> lck(mtx);
-    while (!ready) cv.wait(lck);
-    std::cout << "thread " << id << '\n';
+struct data d;
+
+void print_id () {
+    std::unique_lock<std::mutex> lck(d.mtx);
+    while (!d.ready) d.cv.wait(lck);
+    lck.unlock();
+    std::cout << "thread says hello\n";
 }
 
 void go() {
-    std::unique_lock<std::mutex> lck(mtx);
-    ready = true;
-    cv.notify_all();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::unique_lock<std::mutex> lck(d.mtx);
+    d.ready = true;
+    d.cv.notify_all();
 }
 
 
 void condvar_test(){
     std::cout << "Doing with simple condvar test\n";
-    int num_threads = 2;
+    int num_threads = 1;
     std::thread threads[num_threads];
+    d.ready = false;
     // spawn num_threads threads:
     for (int i=0; i<num_threads; ++i)
-        threads[i] = std::thread(print_id,i);
+        threads[i] = std::thread(print_id);
     std::cout << num_threads <<" threads ready to race...\n";
     go();                       // go!
     // Join the threads
@@ -112,7 +128,7 @@ int main(){
     //std::cout << "int size: " << sizeof(int) << std::endl;
     //std::cout << "spin_lock size: " << sizeof(spin_lock) << std::endl;
     //std::cout << "pthread_mutex_t: " << sizeof(pthread_mutex_t) << std::endl;
-    lock_test();
+    //lock_test();
     //rwlock_test();
-    //condvar_test();
+    condvar_test();
 }
