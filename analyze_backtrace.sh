73d0ed7b04c3ea9358bc74d0f0ddcb90dd32a8c5
Adam Kafka <adkafka@gmail.com>
2017-05-02 12:01:38 -0400

    Analyze a backtrace to produce results

--- analyze_backtrace.sh
+++ analyze_backtrace.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-cat mutex_usage.log | \
+FILE="mutex_usage.log"
+
+if [[ -n $1 ]]; then
+    FILE=$1
+fi
+
+cat $FILE | \
     egrep -o "pthread_cond_[a-z]*|pthread_mutex_[a-z]*|pthread_rwlock_[a-z]*|std::condition_variable::[~a-z]*" |\
     sort | uniq -c
+
+ALL_LOCKS=`cat $FILE | grep "_lock addr: 0x[0-9a-f]*" | sort | uniq -c`
+
+NUM_LOCKS=`echo "$ALL_LOCKS" | wc -l`
+
+TOP_LOCKS=`echo "$ALL_LOCKS" | sort -nr | head -20`
+
+echo "Number of locks: $NUM_LOCKS"
+echo "Top contended locks:"
+echo "$TOP_LOCKS"
